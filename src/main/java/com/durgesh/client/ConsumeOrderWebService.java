7d28fedbf96e049a10b67b99c51a9a2f98b1d6b7
23dgupta <durgeshkumar91g@gmail.com>
2022-08-05 20:36:33 +0530

    enable , disable, find by name, delete,update all done

--- src/main/java/com/durgesh/client/ConsumeOrderWebService.java
+++ src/main/java/com/durgesh/client/ConsumeOrderWebService.java
@@ -1,9 +1,7 @@
 package com.durgesh.client;
 
-
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
@@ -22,97 +20,135 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import com.durgesh.model.OrderDetails;
-import com.durgesh.model.User;
 
 @RestController
 public class ConsumeOrderWebService {
-   @Autowired
-   RestTemplate restTemplate;
+	@Autowired
+	RestTemplate restTemplate;
 
-   // Gel All Users
-	// hello
-	//
-   @GetMapping(value = "/order/getAllOrders")
-   public String getProductList() {
-      HttpHeaders headers = new HttpHeaders();
-      headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
-      HttpEntity <String> entity = new HttpEntity<String>(headers);
-      
-      return restTemplate.exchange("http://localhost:8000/order/getAllOrder", HttpMethod.GET, entity, String.class).getBody();
-   }
-   
-   // Create User
-   @PostMapping(value = "/order/saveOrder")
-   public String createUser(@RequestBody OrderDetails details ) {
-      HttpHeaders headers = new HttpHeaders();
-      headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
-      HttpEntity<OrderDetails> entity = new HttpEntity<OrderDetails>(details,headers);
-      // User addedUser = restTemplate.postForObject("http://localhost:9088/user/create", entity, User.class);
-      // return addedUser;
-      return restTemplate.exchange("http://localhost:8000/order/saveOrder", HttpMethod.POST, entity, String.class).getBody();
-   }
-   
-   // OR
-   
-  /* @PostMapping(value = "/template/createUser")
-   public User createUser(@RequestBody User user) {
-      HttpHeaders headers = new HttpHeaders();
-      headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
-      HttpEntity<User> entity = new HttpEntity<User>(user,headers);
-       User addedUser = restTemplate.postForObject("http://localhost:9088/user/create", entity, User.class);
-       return addedUser;
-     // return restTemplate.exchange("http://localhost:9088/user/create", HttpMethod.POST, entity, String.class).getBody();
-   }
-   */
-   
-   //Update User
-   @PutMapping(value = "/order/updateUser")
-   public String updateUser( @RequestBody User user) {
-	      HttpHeaders headers = new HttpHeaders();
-	      headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
-	      HttpEntity<User> entity = new HttpEntity<User>(user,headers);
-	      
-	      return restTemplate.exchange( "http://localhost:9088/user/updateUser", HttpMethod.PUT, entity, String.class).getBody();
-	   }
-   
-   //Delete User
-   //@RequestMapping(value = "/template/user/{id}", method = RequestMethod.DELETE)
-   @DeleteMapping(value="/order/user/{id}")
-   public String deleteUsert(@PathVariable("id") String id) {
-      HttpHeaders headers = new HttpHeaders();
-      headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
-      HttpEntity<User> entity = new HttpEntity<User>(headers);
-      
-      return restTemplate.exchange(
-         "http://localhost:9088/user/deleteUser/"+id, HttpMethod.DELETE, entity, String.class).getBody();
-   }
-   
-  
-	   
-	 //Get By ID
-	   //@RequestMapping(value = "/template/user/{id}", method = RequestMethod.DELETE)
-	  // @GetMapping(value="/order/search/{customerName}",produces = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE})
-   		@GetMapping(value="/order/ById/{id}")
-	   public OrderDetails getOrderById(@PathVariable("id") String id) {
-		   
-   			ResponseEntity<OrderDetails> response = restTemplate.getForEntity("http://localhost:8000/order/getOneOrder/" +id, OrderDetails.class);
-   			
-   	// ResponseEntity<String> response = restTemplate.exchange(, HttpMethod.GET, headers, String.class,id);
-   	 
-   	// check response
-   	if (response.getStatusCode() == HttpStatus.OK) {
-   	    System.out.println("Request Successful.");
-   	    System.out.println(response.getBody());
-   	} else {
-   	    System.out.println("Request Failed");
-   	    System.out.println(response.getStatusCode());
-   	}
-   	return response.getBody();
-   	}
-       
-	   }
-	   
-	   
-	   
+	// Gel All Users
+	
+	@GetMapping(value = "/order/getAllOrders")
+	public String getProductList() {
+		HttpHeaders headers = new HttpHeaders();
+		headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+		HttpEntity<String> entity = new HttpEntity<String>(headers);
 
+		return restTemplate.exchange("http://localhost:8000/order/getAllOrder", HttpMethod.GET, entity, String.class)
+				.getBody();
+	}
 
+	// Create User
+	@PostMapping(value = "/order/saveOrder")
+	public String createUser(@RequestBody OrderDetails details) {
+		HttpHeaders headers = new HttpHeaders();
+		headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+		HttpEntity<OrderDetails> entity = new HttpEntity<OrderDetails>(details, headers);
+		// User addedUser =
+		// restTemplate.postForObject("http://localhost:9088/user/create", entity,
+		// User.class);
+		// return addedUser;
+		return restTemplate.exchange("http://localhost:8000/order/saveOrder", HttpMethod.POST, entity, String.class)
+				.getBody();
+	}
+
+	
+
+	// Update User
+	@PutMapping(value = "/order/updateOrderDetails/{id}")
+	public String updateUser(@RequestBody OrderDetails orderDetails,@PathVariable("id") Integer id) {
+		HttpHeaders headers = new HttpHeaders();
+		headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+		HttpEntity<OrderDetails> entity = new HttpEntity<OrderDetails>(orderDetails, headers);
+
+		return restTemplate.exchange("http://localhost:8000/order/updateOrder/"+id, HttpMethod.PUT, entity, String.class)
+				.getBody();
+	}
+
+	// Delete User
+	// @RequestMapping(value = "/template/user/{id}", method = RequestMethod.DELETE)
+	@DeleteMapping(value = "/deleteOrder/{id}")
+	public String deleteUsert(@PathVariable("id") String id) {
+		HttpHeaders headers = new HttpHeaders();
+		headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+		HttpEntity<OrderDetails> entity = new HttpEntity<OrderDetails>(headers);
+
+		 ResponseEntity<String> response = restTemplate.exchange("http://localhost:8000/order/deleteorder/" + id, HttpMethod.DELETE, entity, String.class);
+		 
+		// check response
+			if (response.getStatusCode() == HttpStatus.OK) {
+				System.out.println("Request Successful.");
+				System.out.println(response.getBody());
+			} else {
+				System.out.println("Request Failed");
+				System.out.println(response.getStatusCode());
+			}
+			return response.getBody();
+				
+	}
+
+	// Get By ID
+	// @RequestMapping(value = "/template/user/{id}", method = RequestMethod.DELETE)
+	// @GetMapping(value="/order/search/{customerName}",produces =
+	// {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE})
+	@GetMapping(value = "/order/ById/{id}")
+	public OrderDetails getOrderById(@PathVariable("id") String id) {
+
+		ResponseEntity<OrderDetails> response = restTemplate
+				.getForEntity("http://localhost:8000/order/getOneOrder/" + id, OrderDetails.class);
+
+		// check response
+		if (response.getStatusCode() == HttpStatus.OK) {
+			System.out.println("Request Successful.");
+			System.out.println(response.getBody());
+		} else {
+			System.out.println("Request Failed");
+			System.out.println(response.getStatusCode());
+		}
+		return response.getBody();
+	}
+	
+	// DisableProductById
+	@GetMapping(value ="/order/disableProductById/{id}")
+	public void disableProduct(@PathVariable("id") Integer id) {
+
+		 restTemplate
+				.put("http://localhost:8000/order/disableProduct/" + id, String.class);
+
+		
+	}
+	
+	// EnableProductById
+	@GetMapping(value ="/order/enableProductById/{id}")
+		public void enableProduct(@PathVariable("id") Integer id) {
+
+		restTemplate
+					.put("http://localhost:8000/order/enableProduct/" + id, String.class);
+
+			
+		}
+	
+	
+	
+	
+	// Get By customerName
+		
+		@GetMapping(value = "/order/getBycustomerName/{customerName}")
+		public List<OrderDetails> getBycustomerName(@PathVariable("customerName") String customerName) {
+
+			ResponseEntity<OrderDetails[]> response = restTemplate.getForEntity("http://localhost:8000/order/getAllOrderName/"+customerName, OrderDetails[].class);
+			return Arrays.asList(response.getBody());
+			
+		}
+
+		
+		// Get By customerName
+		
+				@GetMapping(value = "/order/searchBycustomerName/{customerName}")
+				public List<Object> searchBycustomerName(@PathVariable("customerName") String customerName) {
+
+					ResponseEntity<Object[]> response = restTemplate.getForEntity("http://localhost:8000/order/search/"+customerName, Object[].class);
+					return Arrays.asList(response.getBody());
+					
+				}
+}
